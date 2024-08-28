f010e9db01f9620887c01b63bfb262301a4c4fa3
우태진 <taejin.woo@vtw.co.kr>
2024-08-28 17:07:36 +0900

    메타 파일 디렉터리 변경

--- src/main/java/com/vtw/dna/camel/configuration/CamelConfiguration.java
+++ src/main/java/com/vtw/dna/camel/configuration/CamelConfiguration.java
@@ -2,6 +2,7 @@ package com.vtw.dna.camel.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.vtw.dna.camel.DnaExchange;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.apache.camel.CamelContext;
@@ -43,21 +44,23 @@ public class CamelConfiguration {
     }
 
     private void buildRouteVariables(CamelContext camelContext) throws IOException {
-        try (Stream<Path> paths = Files.list(templateParametersDirectory)) {
-            paths.forEach(path -> {
-                try {
-                    String yaml = Files.readString(path);
-                    List<Map<String, Object>> list = new ObjectMapper(new YAMLFactory()).readValue(yaml, List.class);
+        Files.find(templateParametersDirectory,
+                        Integer.MAX_VALUE,
+                        (filePath, fileAttr) -> fileAttr.isRegularFile())
+                .filter(path -> path.toString().endsWith(".templated.yaml"))
+                .forEach(path -> {
+                    try {
+                        String yaml = Files.readString(path);
+                        List<Map<String, Object>> list = new ObjectMapper(new YAMLFactory()).readValue(yaml, List.class);
 
-                    for (Map<String, Object> map : list) {
-                        String flowId = (String) map.get("flowId");
-                        Map<String, Object> parameters = Collections.unmodifiableMap((Map<String, Object>) map.get("parameters"));
-                        camelContext.setVariable("route:" + flowId + ":DnaTemplateParameters", parameters);
+                        for (Map<String, Object> map : list) {
+                            String flowId = (String) map.get("flowId");
+                            Map<String, Object> parameters = Collections.unmodifiableMap((Map<String, Object>) map.get("parameters"));
+                            camelContext.setVariable("route:" + flowId + ":" + DnaExchange.TEMPLATE_PARAMETERS, parameters);
+                        }
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
                     }
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            });
-        }
+                });
     }
 }
